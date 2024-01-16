1c416732e9e606320bd4b7201a83035907bb5673
maitracle <maitracle@gmail.com>
2024-01-17 00:09:25 +0900

    add fail test with empty data for TodoCard

--- src/test/kotlin/com/teamsparta/todo/todocards/TodoCardTest.kt
+++ src/test/kotlin/com/teamsparta/todo/todocards/TodoCardTest.kt
@@ -1,6 +1,7 @@
 package com.teamsparta.todo.todocards
 
 import com.teamsparta.todo.replies.Reply
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
@@ -12,7 +13,7 @@ class TodoCardTest : BehaviorSpec({
         val authorName = "authorName"
         val replies = emptyList<Reply>()
 
-        When("excute TodoCard constructor") {
+        When("execute TodoCard constructor") {
             val result = TodoCard(
                 id = id,
                 title = title,
@@ -27,6 +28,54 @@ class TodoCardTest : BehaviorSpec({
                 result.content shouldBe content
                 result.authorName shouldBe authorName
                 result.replies shouldBe replies
+            }
+        }
+    }
+
+    Given("information of todo card with empty title") {
+        val id = null
+        val title = ""
+        val content = "content"
+        val authorName = "authorName"
+        val replies = emptyList<Reply>()
+
+        When("execute TodoCard constructor") {
+            val result = shouldThrow<TodoCardException> {
+                TodoCard(
+                    id = id,
+                    title = title,
+                    content = content,
+                    authorName = authorName,
+                    replies = replies,
+                )
+            }
+
+            Then("thrown exception message should be expected") {
+                result.message shouldBe "title must be at least 1 character and not more than 200 characters long"
+            }
+        }
+    }
+
+    Given("information of todo card with empty content") {
+        val id = null
+        val title = "title"
+        val content = ""
+        val authorName = "authorName"
+        val replies = emptyList<Reply>()
+
+        When("execute TodoCard constructor") {
+            val result = shouldThrow<TodoCardException> {
+                TodoCard(
+                    id = id,
+                    title = title,
+                    content = content,
+                    authorName = authorName,
+                    replies = replies,
+                )
+            }
+
+            Then("thrown exception message should be expected") {
+                result.message shouldBe "content must be at least 1 character and not more than 1000 characters long"
             }
         }
     }
