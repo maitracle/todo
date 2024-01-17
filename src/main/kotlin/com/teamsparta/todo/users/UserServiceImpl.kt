ccc2b67f7086da314da30e57ef8d883e5a1e7f81
maitracle <maitracle@gmail.com>
2024-01-17 22:14:26 +0900

    encode password when create user

--- src/main/kotlin/com/teamsparta/todo/users/UserServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/todo/users/UserServiceImpl.kt
@@ -2,14 +2,21 @@ package com.teamsparta.todo.users
 
 import com.teamsparta.todo.users.dtos.CreateUserArguments
 import com.teamsparta.todo.users.dtos.UserDto
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserServiceImpl(
     val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder, // 주입!
 ) : UserService {
     override fun createUser(createUserArguments: CreateUserArguments): UserDto {
-        val createdUser = userRepository.save(createUserArguments.to())
+        val user = User(
+            username = createUserArguments.username,
+            password = passwordEncoder.encode(createUserArguments.password),
+        )
+
+        val createdUser = userRepository.save(user)
 
         return UserDto.from(createdUser)
     }
